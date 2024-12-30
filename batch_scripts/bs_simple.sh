7c804ea350733c24d460f2368c6d5f4b3b7441b7
AxMeNi <axel.mengelle@gmail.com>
2024-12-30 16:10:41 +0800

    Updated batch file to make simpler trials

--- batch_scripts/bs_simple.sh
+++ batch_scripts/bs_simple.sh
@@ -3,8 +3,5 @@
 #SBATCH --job-name=geoclassification_mps
 #SBATCH --cpus-per-task=4
 #SBATCH --output=log/test_array_job_task_%a.txt
-#SBATCH --array=1-30
-cp path/to/interface.py path/to/interface_${SLURM_ARRAY_TASK_ID}.py
-sed -i "s/seed = 852/seed = ${SLURM_ARRAY_TASK_ID}/" path/to/interface_${SLURM_ARRAY_TASK_ID}.py
-python path/to/interface_${SLURM_ARRAY_TASK_ID}.py
-rm path/to/interface_${SLURM_ARRAY_TASK_ID}.py
+
+python path/to/main.py -s
