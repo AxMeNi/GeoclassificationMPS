91c07571d35f3d7eb6bd6d78f650b7e27d83c5b6
AxMeNi <axel.mengelle@gmail.com>
2024-12-30 16:20:17 +0800

    Updated batch file to remove a in the job name

--- batch_scripts/bs_simple.sh
+++ batch_scripts/bs_simple.sh
@@ -2,6 +2,6 @@
 #SBATCH --partition=work
 #SBATCH --job-name=geoclassification_mps
 #SBATCH --cpus-per-task=4
-#SBATCH --output=log/test_array_job_task_%a.txt
+#SBATCH --output=log/test_array_job_task.txt
 
 python path/to/main.py -s
